87635907cf99292472ca9547e9cffe298d2f1e02
shadowElectric <lcrandal@andrew.cmu.edu>
2015-02-07 18:53:50 -0500

    Re Sync Training

--- hw2/hw2.cpp
+++ hw2/hw2.cpp
@@ -21,41 +21,25 @@
 using namespace cv;
 using namespace std;
 
-
-struct image_source{
-    vector<Mat>* data; // matrices from all classes in order
-    int num_per_class; // number of matrices per class
-};
-
-struct pc{
-    Mat& eigvec;
-    Mat& eigval;
-    Mat& mean;
-    int label;
-};
-
-typedef struct pc pc;
-typedef struct image_source imsrc;
-
 /* Macros */
 #define READING "r"
 #define WRITING "w"
 #define MAXLEN 40
-#define CLASS_POS 6
+#define EIGENSIZE 16385
 
 /* Enable when debugging */
 #define DEBUG (1)
 
 /* Function prototypes */
 void DisplayMat(Mat MatDisp);
-void path_to_data(string p, imsrc* dst);
+vector<Mat>* path_to_data(string p);
 
 
 
 /* define program argument list, excluding flags */
-enum {ARG_PROG, ARG_FILE, ARG_NARGS};
+enum {ARG_PROG, ARG_TRAINPATH, ARG_TESTPATH, ARG_OUTFILE, ARG_NARGS};
         
-int main(int argc, char* argv[])
+int main()
 {
 
 	// Call IPCAtrain to generate a text file containing eigenvectors and means of each clas
@@ -63,107 +47,21 @@ int main(int argc, char* argv[])
 	// Call IPCAtest using a test function and see if it returns the correct class label
 
 	/* STAGE 1: variable initialization */
-    FILE* ofp = NULL;
-    imsrc train = {NULL,0}; 
-    imsrc* trainp = NULL;
-    imsrc test = {NULL,0};
-    imsrc* testp = NULL;
-    vector<pc>* trained = NULL;
-    string train_path = "";
-    string test_path = "";
     int error = 0;
-    int train_flag = 0;
-    int test_flag = 0;
+    vector<Mat>* train = NULL;
+    int train_size = 0;
     
     
     /* STAGE 2: input processing (if any) */
-    printf("hw2: train data from [train-path] and test data from [test-path].\n");
-    
-    
-	/* one optional flag: -wN */
-	if(argc > 1){
-		char flag;
-		while(argv[1][0] == '-'){
-			flag = argv[1][1];
-			switch(flag){
-			/*TODO: handle any  flag arguments here */
-			case('\0'):{
-				printf("Error: missing flag name\n");
-				return 1;
-			}
-			case('r'):{// training path
-			    train_path = string(&argv[1][2]);
-                train_flag = 1;
-                break;
-			} 
-
-            case('e'):{ // testing path
-                test_path = string(&argv[1][2]);
-                test_flag = 1;
-                break;
-            }
-			default:
-				break;
-			}
-			argc--;
-			argv++;
-		}
-	}
-    
-    if (argc < ARG_NARGS){
-		printf("insufficient arguments.\n"
-			"usage: hw2 [-tTRAINING_PATH][-eTESTING_PATH] FILENAME\n"
-            "\tTRAINING_PATH : full path to the training directory"
-                " e.g. ./Train_Files/\n"
-			"\tTESTING_PATH  : full path to the testing directory"
-                " e.g. ./Test_Files/\n"
-			"\tFILENAME      : output filename in case of training; "
-            "input filename in case of testing\n"
-			);
-		return 1;
-    }
-    
+    string p = "./Train_Files/";
+    train = path_to_data(p);
 #if DEBUG
-    cout << "train_path = " << train_path << "\n";
-    cout << "test_path = " << test_path << "\n";
-#endif    
-    
-    ofp = fopen(argv[ARG_FILE],WRITING);
-    if (ofp == NULL){
-		printf("Error: unable to open %s\n", argv[ARG_FILE]);
-		error++;
-		goto exit;
-    }
-    //train_path = "./Train_Files/"; //TODO: get path from command line
-    if (train_flag){
-        trainp = &train;
-         path_to_data(train_path,trainp);
-    }
-       
-    //test_path = "./Test_Files"; //TODO: get path from command line
-    if (test_flag){
-        testp = &test;
-        path_to_data(test_path,testp);
-    }
-        
-#if DEBUG
-    if (trainp != NULL){
-        printf ("train has %lu samples total.\n",trainp->data->size());
-        printf("Each class has %d samples.\n", trainp->num_per_class);
-    }
-    if (testp != NULL){
-        printf ("test has %lu samples total.\n",testp->data->size());
-        printf("Each class has %d samples.\n", testp->num_per_class);
-    }
-    else {
+    if (train != NULL)
+        printf ("train has size %lu\n",train->size());
+    else
         printf("error!\n");
-        goto exit;
-    }
 #endif
-    
 	/* STAGE 3: actual processing */
-    //trained = IPCAtrain(tain);
-    
     /* STAGE 4: output processing (if any) */
 
 
@@ -172,25 +70,38 @@ int main(int argc, char* argv[])
 exit:
     printf("hw2 quitting...\n");
     /* close any file descripter */
-    if (ofp != NULL)
-        fclose(ofp);
+    
     /* free any occupied memory */
-    if ((trainp != NULL) && (trainp->data != NULL))
-        trainp->data->clear();
-    if ((testp != NULL) && (testp->data != NULL))
-        testp->data->clear();
+    if (train != NULL)
+        train->clear();
 	return error;
 }
-void IPCAtrain(char* trainFolderPath, int numTrain)
+void IPCAtrain(imsrc *images, pc *output)
 {
 	/* trainFolderPath is the path to the folder containing the training images
 	   numTrain is the number of training images per class */
-
-	// Run a loop to iterate over classes (people)
-	for(;;)
-	{
-		//Run a loop to iterate over images of same person and generate the data matrix for the class 
-		//i.e. a matrix in which each column is a vectorized version of the face matrix under consideration
+  
+  int data_size = images->data->size();
+  
+  if((data_size % images->num_per_class) != 0){
+      return null;
+  }
+  int num_classes = data_size / images->num_num_per_class;
+  vector<Mat>* data_mat = (num_classes, Mat(EIGENSIZE,images->num_train,CV_32FC1));
+  vector<Mat>& covar_mat = (num_classes, Mat(EIGENSIZE,EIGENSIZE,CV_32FC1));
+			
+  // Run a loop to iterate over classes (people)
+  for(int i = 0; i < num_classes; i++){
+    //Run a loop to iterate over images of same person and generate the data matrix for the class
+    //i.e. a matrix in which each column is a vectorized version of the face matrix under consideration
+    for(int j = 0; j < images->num_per_class; j++){
+      data_mat->at(i)->row(j) = images->data[(i * num_classes)+j]->reshape(1,1);
+    }
+    calcCovarMatrix(data_mat->at(i),images->num_per_class, covar_mat->at(i), output->at(i)->mean, CV_COVAR_NORMAL | CV_COVAR_COLS, CV_32FC1);
+    eigen(covar_mat->at(i),1,output->at(i)->eigvec,output->at(i)->eigval);
+    output->at(i)->label = i;
+  }
+  
 
 		// Subtract the mean vector from each vector of the data matrix
 
@@ -219,38 +130,30 @@ void IPCAtest(char *imgName)
 
 /* find the paths of all the files in p, 
    and construct dst from all images in p */
-void path_to_data(string p, imsrc* dst){
+vector<Mat>* path_to_data(string p){
 	DIR* dir = NULL;
 	struct dirent *entry = NULL;
     string* paths = new string[MAXLEN];
     int size = 0;
-    int num_per_class = 0;
-    int flag = 1;
-    int plen = p.length();
-    int offset = plen + CLASS_POS-1; // index of class name in full path
     if((dir = opendir(p.c_str())) != NULL){
         while((entry = readdir(dir)) != NULL){
             if( strcmp(entry->d_name, ".") != 0 
                     && strcmp(entry->d_name, "..") != 0){
                 paths[size] = p + string(entry->d_name);
-                if (flag && ((!size) || 
-                    (paths[size][offset] == paths[size-1][offset]))){
-                    num_per_class++;
-                }
-                else
-                    flag = 0;
-                size++;
+                size += 1;
             }
         }
-        vector<Mat>* d = new vector<Mat>(size);
+#if DEBUG
+        printf("size:%d\n",size);
+#endif
+        vector<Mat>* data = new vector<Mat>(size);
         for (int i = 0; i < size; i++){
-            d->at(i) = imread(paths[i]);
+            data->at(i) = imread(paths[i]);
         }
-        dst->data = d;
-        dst->num_per_class = num_per_class;
 		closedir(dir);
-        return;
+        return data;
 	}
+    return NULL;
 }
 
 /* brought from Sample_Code.cpp */
